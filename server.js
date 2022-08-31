a17c3bb497a5939242bbae6bb232292bb3317e42
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-08-31 08:12:20 -0500

    add deployment to heroku

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.PORT || 3000;
+
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
@@ -15,5 +16,5 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
-    console.log(`Server live on ${ PORT } !`)
-});
+    console.log(`API server now on port ${PORT}!`);
+  });
