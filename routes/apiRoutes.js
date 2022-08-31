5f5aecc212ab4ec1659ccb5c3ffd97bdc65572d6
Poison512 <adrian512@Adrians-MBP.attlocal.net>
2022-08-30 21:01:32 -0500

    fixing routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,5 +3,25 @@ const router = express.Router();
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+router.get('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (error, data) => {
+        if (error) {
+            res.status(500).json('500');
+            console.log('Error!!!')
+        };
+        res.json(JSON.parse(data));
+    });
+});
+
+router.post ('/notes', (req, res) => {
+    fs.readFile('./db/db.json', 'utf-8', (error,data) => {
+        if (error) {
+            res.status(500).json('500');
+        };
+        let newData = JSON.parse(data)
+        newData.push({...req.body, id:uuidv4() });
+
+    })
+})
 
 module.exports = router;
